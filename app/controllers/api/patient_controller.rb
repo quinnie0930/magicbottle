df034b67d68f7e1bdf4ab964a088609189003acc
Quynh Pham <quynhpham@gmail.com>
2015-03-08 14:14:25 +0700

    fix migration errors

--- app/controllers/api/patient_controller.rb
+++ app/controllers/api/patient_controller.rb
@@ -1,9 +1,15 @@
 class Api::PatientController < ApplicationController
-   def create
-    if user = User.create(username: params[:username])
-      render json: { userid: user.id, username: user.username }
+  def create
+    if patient = Patient.create(patient_params)
+      render json: { userid: patient.id, username: patient.username }
     else
       render json: {}, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :lastname, :username, :phone_number)
   end
 end
--- db/migrate/20150308015136_create_feedbacks.rb
+++ db/migrate/20150308015136_create_feedbacks.rb
@@ -2,7 +2,8 @@ class CreateFeedbacks < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
       t.integer :pill_id
-      t.stringtime_after_pill_taken :comment
+      t.datetime :time_after_pill_taken
+      t.text :comment
       t.integer :rating
 
       t.timestamps null: false
