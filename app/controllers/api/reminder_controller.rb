d24675ad71cdbcdd31ca0758fbf77842995b5ee2
Quynh Pham <Quinnie@Quynhs-MacBook-Air.local>
2015-03-07 22:21:59 -0800

    some controllers

--- app/controllers/api/reminder_controller.rb
+++ app/controllers/api/reminder_controller.rb
@@ -1,2 +1,13 @@
-class Api::ReminderController < ApplicationController
-end
+# class Api::ReminderController < ApplicationController
+#   def remind
+#      current_time = Time.new.to_i
+#      if params[:user_id].present?
+#         current_user = User.find(:id params[:user_id])
+#         if (current_time-current_user.pill.reminder.reminder_time).abs>=1000
+#           return
+#           #true
+#      end
+
+#   end
+
+# end
--- db/migrate/20150308015136_create_feedbacks.rb
+++ db/migrate/20150308015136_create_feedbacks.rb
@@ -2,7 +2,8 @@ class CreateFeedbacks < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
       t.integer :pill_id
-      t.stringtime_after_pill_taken :comment
+      t.string :comment
+      t.integer :time_after_pill_taken
       t.integer :rating
 
       t.timestamps null: false
